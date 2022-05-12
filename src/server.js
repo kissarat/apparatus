6e6f272391064686ac592ae089b87b8bfe4952a6
Taras Labiak <kissarat@gmail.com>
2022-05-12 14:52:52 +0300

    + express.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
+const express = require('express')
+const http = require('http')
+
+function home(req, res) {
+    res.json({
+        message: 'Hello World!'
+    })
+}
+
 function main() {
     try {
-        console.log('Hello World!')
+        const app = express()
+        app.get('/', home)
+        const server = http.createServer(app)
+        const port = +process.env.PORT || 3000
+        server.listen(port, function() {
+            console.log(`Listen at http://localhost:${port}/`)
+        })
+
     } catch (err) {
         console.error(err)
     }
